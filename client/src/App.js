852e003e3fc6240490714820e54ec58454058042
Mateo-Rey <jrey3darts@gmail.com>
2022-11-15 10:11:49 -0500

    socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+import Options from "./components/Options.jsx";
+import VideoPlayer from "./components/VideoPlayer.jsx";
+import Notifications from "./components/Notifications.jsx";
+import { Typography, AppBar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 100px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "600px",
+    border: "2px solid black",
+
+    [theme.breakpoints.down("xs")]: {
+      width: "90%",
+    },
+  },
+  image: {
+    marginLeft: "15px",
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+}));
 const App = () => {
+    const classes = useStyles();
   return (
-    <div>
-        Video Chat App
-    </div>
-  )
-}
+    <div className={classes.wrapper}>
+      <AppBar className={classes.appBar} position="static" color="default">
+        <Typography variant="h2" align="center">
+          Video Chat
+        </Typography>
+      </AppBar>
 
-export default App
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+    </div>
+  );
+};
+
+export default App;
